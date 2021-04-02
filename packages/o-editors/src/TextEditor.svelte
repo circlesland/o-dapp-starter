0b2c64be3f2135b89de956a1049b2746d9be0cb0
Thorsten Schau <letsgo@codeho.net>
2021-04-02 16:22:05 +0200

    temporarily show all buttons for styling, base color changes

--- packages/o-editors/src/TextEditor.svelte
+++ packages/o-editors/src/TextEditor.svelte
@@ -27,29 +27,29 @@
 <div
   class="flex mt-4 space-x-0 md:space-x-4 space-y-4 md:space-y-0 flex-col md:flex-row"
 >
-  {#if context.canGoBack}
-    <div class="flex-1 ">
-      <button
-        on:click={() => context.process.sendAnswer(new Back())}
-        class="btn btn-secondary btn-block"
+  <!-- {#if context.canGoBack} -->
+  <div class="flex-1 ">
+    <button
+      on:click={() => context.process.sendAnswer(new Back())}
+      class="btn btn-outline btn-secondary btn-block"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="inline-block w-6 h-6 ml-2 stroke-current"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block w-6 h-6 ml-2 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M15 19l-7-7 7-7"
-          />
-        </svg>
-        Go back</button
-      >
-    </div>
-  {/if}
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M15 19l-7-7 7-7"
+        />
+      </svg>
+      Go back</button
+    >
+  </div>
+  <!-- {/if} -->
   <div class="flex-1">
     <button
       on:click={() => {
@@ -75,29 +75,29 @@
     </button>
   </div>
 
-  {#if context.canSkip}
-    <div class="flex-1">
-      <button
-        on:click={() => context.process.sendAnswer(new Skip())}
-        class="btn btn-outline btn-secondary btn-block"
+  <!-- {#if context.canSkip} -->
+  <div class="flex-1">
+    <button
+      on:click={() => context.process.sendAnswer(new Skip())}
+      class="btn btn-outline btn-primary btn-block"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="inline-block w-6 h-6 mr-2 stroke-current"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block w-6 h-6 mr-2 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-        Skip
-      </button>
-    </div>
-  {/if}
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+      Skip
+    </button>
+  </div>
+  <!-- {/if} -->
 </div>
 <div>
   <small>
