266f0db070c5095ccfcd55f256984c6a5f244792
Samuel Andert <samuelandert@me.com>
2021-04-06 00:41:19 +0200

    fixed footer nav css

--- shell/src/App.svelte
+++ shell/src/App.svelte
@@ -25,14 +25,14 @@
   import Success from "./shared/atoms/Success.svelte";
   import { Generate } from "@o-platform/o-utils/dist/generate";
   import { Subscription } from "rxjs";
-  import {Prompt} from "@o-platform/o-process/dist/events/prompt";
-  import {Back} from "@o-platform/o-process/dist/events/back";
-  import {Skip} from "@o-platform/o-process/dist/events/skip";
-  import {ProcessEvent} from "@o-platform/o-process/dist/interfaces/processEvent";
+  import { Prompt } from "@o-platform/o-process/dist/events/prompt";
+  import { Back } from "@o-platform/o-process/dist/events/back";
+  import { Skip } from "@o-platform/o-process/dist/events/skip";
+  import { ProcessEvent } from "@o-platform/o-process/dist/interfaces/processEvent";
 
   let isOpen: boolean = false;
   let modalProcess: Process;
-  let modalProcessEventSubscription:Subscription;
+  let modalProcessEventSubscription: Subscription;
 
   let progressIndicator: {
     message: string;
@@ -57,14 +57,20 @@
         // If the process should be started modal, let App.svelte's ProcessContainer handle it.
         modalProcess = runningProcess;
         isOpen = true;
-        modalProcessEventSubscription = modalProcess.events.subscribe((processEvent:ProcessEvent) => {
-          if(processEvent.event.type == "process.ipc.bubble"
-            && (<any>processEvent.event).wrappedEvent.type == "process.prompt")
-          {
-            console.log("lastPrompt:", (<any>processEvent.event).wrappedEvent);
-            lastPrompt = <Prompt>(<any>processEvent.event).wrappedEvent;
+        modalProcessEventSubscription = modalProcess.events.subscribe(
+          (processEvent: ProcessEvent) => {
+            if (
+              processEvent.event.type == "process.ipc.bubble" &&
+              (<any>processEvent.event).wrappedEvent.type == "process.prompt"
+            ) {
+              console.log(
+                "lastPrompt:",
+                (<any>processEvent.event).wrappedEvent
+              );
+              lastPrompt = <Prompt>(<any>processEvent.event).wrappedEvent;
+            }
           }
-        });
+        );
       } else {
         // If not, send an event with the process id.
         const startedEvent = new ProcessStarted(runningProcess.id);
@@ -97,7 +103,7 @@
     // TODO: Cannot currently remember what this callback does. Lookup documentation.
   }
 
-  let lastPrompt: Prompt|undefined = undefined;
+  let lastPrompt: Prompt | undefined = undefined;
 
   function routeLoading() {
     // Pretty self explanatory. For more lookup the svelte-spa-router docs,
@@ -135,34 +141,38 @@
   }
 </script>
 
-<div class="grid grid-cols-1 bg-gray-100">
-  <div class="flex h-screen">
+<div class="flex flex-col h-screen bg-gray-100">
+  <main class="flex-1 overflow-y-auto">
     <Router
       {routes}
       on:conditionsFailed={conditionsFailed}
       on:routeLoading={routeLoading}
     />
-
-    <div
-      style="position:absolute; bottom:0; left:50%; margin-left:-125px; width:250px; z-index:9999999;"
-    >
+  </main>
+  <footer class="z-50 flex justify-center w-full">
+    <div class="flex space-x-2">
       {#if lastPrompt && lastPrompt.navigation.canGoBack}
-        <button class="bg-white btn btn-outline"
-                on:click={() => modalProcess.sendAnswer(new Back())}>back</button>
+        <button
+          class="bg-white btn btn-outline"
+          on:click={() => modalProcess.sendAnswer(new Back())}>back</button
+        >
       {/if}
       {#if !localStorage.getItem("circles.key")}
-      <button class="mb-4 bg-white btn btn-outline"
-              on:click={() => authenticateWithCircles("circles.land")}>
-        <img
-          width="15px"
-          class="mr-3"
-          src="/images/common/circles.png"
-          alt="circles.land"
-        /> login with circles</button>
+        <button
+          class="mb-4 bg-white btn btn-outline"
+          on:click={() => authenticateWithCircles("circles.land")}
+        >
+          <img
+            width="15px"
+            class="mr-3"
+            src="/images/common/circles.png"
+            alt="circles.land"
+          /> login with circles</button
+        >
       {:else}
         <button
           className="bottom-0 p-3 bg-white border border-black rounded-full"
-          on:click={() => isOpen = !isOpen}
+          on:click={() => (isOpen = !isOpen)}
         >
           <img
             width="40px"
@@ -172,12 +182,13 @@
         </button>
       {/if}
       {#if lastPrompt && lastPrompt.navigation.canSkip}
-        <button class="bg-white btn btn-outline"
-                on:click={() => modalProcess.sendAnswer(new Skip())}>skip</button>
+        <button
+          class="bg-white btn btn-outline"
+          on:click={() => modalProcess.sendAnswer(new Skip())}>skip</button
+        >
       {/if}
     </div>
-
-  </div>
+  </footer>
 </div>
 
 <Modal bind:isOpen on:closeRequest={modalWantsToClose}>
