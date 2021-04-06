5896c61ba540520b249c24b3c5df32de89ee545a
Samuel Andert <samuelandert@me.com>
2021-04-06 04:01:38 +0200

    added dummy ui to banking dapp

--- shell/src/dapps/o-banking/pages/Trusts.svelte
+++ shell/src/dapps/o-banking/pages/Trusts.svelte
@@ -1,5 +1,37 @@
 <script lang="ts">
 </script>
-<br/>
-Trusts<br/>
-<br/>
+
+<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">TRUSTS</div>
+
+<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">
+  NEW INCOMING TRUST
+</div>
+
+<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">
+  Name 1 <button class="btn">send money</button><button class="btn"
+    >trust back</button
+  >
+</div>
+
+<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">MUTUTAL TRUST</div>
+
+<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">
+  Name 2 <button class="btn">send money</button><button class="btn"
+    >remove trust</button
+  >
+</div>
+<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">TRUSTED BY YOU</div>
+
+<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">
+  Name 3 <button class="btn">send money</button><button class="btn"
+    >remove trust</button
+  >
+</div>
+
+<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">REMOVED TRUST</div>
+
+<div class="p-4 mt-4 bg-white rounded-t-xl md:rounded-xl">
+  Name 4 <button class="btn">send money</button><button class="btn"
+    >add trust</button
+  >
+</div>
--- shell/src/dapps/o-banking/processes/transfer.ts
+++ shell/src/dapps/o-banking/processes/transfer.ts
@@ -2,21 +2,21 @@ import { ProcessDefinition } from "@o-platform/o-process/dist/interfaces/process
 import { ProcessContext } from "@o-platform/o-process/dist/interfaces/processContext";
 import { fatalError } from "@o-platform/o-process/dist/states/fatalError";
 import { createMachine } from "xstate";
-import {prompt} from "@o-platform/o-process/dist/states/prompt";
+import { prompt } from "@o-platform/o-process/dist/states/prompt";
 import TextEditor from "../../../../../packages/o-editors/src/TextEditor.svelte";
 import CurrencyTransfer from "../../../../../packages/o-editors/src/CurrencyTransfer.svelte";
-import {CloseModal} from "@o-platform/o-events/dist/shell/closeModal";
-import {CreateOrRestoreKeyContext} from "../../o-passport/processes/createOrRestoreKey";
-import {ipc} from "@o-platform/o-process/dist/triggers/ipc";
-import {transferXdai} from "./transferXdai";
-import {transferCircles} from "./transferCircles";
-import {Cancel} from "@o-platform/o-process/dist/events/cancel";
-import {PlatformEvent} from "@o-platform/o-events/dist/platformEvent";
+import { CloseModal } from "@o-platform/o-events/dist/shell/closeModal";
+import { CreateOrRestoreKeyContext } from "../../o-passport/processes/createOrRestoreKey";
+import { ipc } from "@o-platform/o-process/dist/triggers/ipc";
+import { transferXdai } from "./transferXdai";
+import { transferCircles } from "./transferCircles";
+import { Cancel } from "@o-platform/o-process/dist/events/cancel";
+import { PlatformEvent } from "@o-platform/o-events/dist/platformEvent";
 
 export type TransferCirclesContextData = {
-  safeAddress:string;
-  recipientAddress?:string;
-  tokens?:{
+  safeAddress: string;
+  recipientAddress?: string;
+  tokens?: {
     currency: {
       key: string,
       label: string
@@ -36,96 +36,96 @@ export type TransferCirclesContext = ProcessContext<TransferCirclesContextData>;
  * In case you want to translate the flow later, it's nice to have the strings at one place.
  */
 const strings = {
-  labelRecipientAddress: "",
+  labelRecipientAddress: "Select the recipient you want to send money to (autocomplete up)",
   tokensLabel: "Please enter the amount and the token you want to transfer",
   currencyCircles: "CRC",
   currencyXdai: "xDai"
 };
 
 const processDefinition = (processId: string) =>
-createMachine<TransferCirclesContext, any>({
-  id: `${processId}:transfer`,
-  initial: "recipientAddress",
-  states: {
-    // Include a default 'error' state that propagates the error by re-throwing it in an action.
-    // TODO: Check if this works as intended
-    ...fatalError<TransferCirclesContext, any>("error"),
+  createMachine<TransferCirclesContext, any>({
+    id: `${processId}:transfer`,
+    initial: "recipientAddress",
+    states: {
+      // Include a default 'error' state that propagates the error by re-throwing it in an action.
+      // TODO: Check if this works as intended
+      ...fatalError<TransferCirclesContext, any>("error"),
 
-    recipientAddress: prompt<TransferCirclesContext, any>({
-      fieldName: "recipientAddress",
-      component: TextEditor,
-      params: {
-        label: strings.labelRecipientAddress,
-      },
-      navigation: {
-        next: "#tokens",
-      },
-    }),
-    tokens: prompt<CreateOrRestoreKeyContext, any>({
-      fieldName: "tokens",
-      component: CurrencyTransfer,
-      params: {
-        label: strings.tokensLabel,
-        currencies:[{
-          key: "crc",
-          label: strings.currencyCircles
-        },{
-          key: "xdai",
-          label: strings.currencyXdai
-        }]
-      },
-      navigation: {
-        next: "#checkChoice",
-      },
-    }),
-    checkChoice: {
-      id: "checkChoice",
-      always: [
-        {
-          cond: (context) => {
-            return context.data.tokens.currency.key == "crc"
-          },
-          target: "callCirclesTransfer",
+      recipientAddress: prompt<TransferCirclesContext, any>({
+        fieldName: "recipientAddress",
+        component: TextEditor,
+        params: {
+          label: strings.labelRecipientAddress,
         },
-        {
-          cond: (context) => {
-            return context.data.tokens.currency.key == "xdai"
-          },
-          target: "callXdaiTransfer",
+        navigation: {
+          next: "#tokens",
         },
-      ],
-    },
-    callCirclesTransfer: {
-      id: "callCirclesTransfer",
-      on: <any>{
-        ...ipc("callCirclesTransfer"),
+      }),
+      tokens: prompt<CreateOrRestoreKeyContext, any>({
+        fieldName: "tokens",
+        component: CurrencyTransfer,
+        params: {
+          label: strings.tokensLabel,
+          currencies: [{
+            key: "crc",
+            label: strings.currencyCircles
+          }, {
+            key: "xdai",
+            label: strings.currencyXdai
+          }]
+        },
+        navigation: {
+          next: "#checkChoice",
+        },
+      }),
+      checkChoice: {
+        id: "checkChoice",
+        always: [
+          {
+            cond: (context) => {
+              return context.data.tokens.currency.key == "crc"
+            },
+            target: "callCirclesTransfer",
+          },
+          {
+            cond: (context) => {
+              return context.data.tokens.currency.key == "xdai"
+            },
+            target: "callXdaiTransfer",
+          },
+        ],
       },
-      invoke: {
-        src: transferCircles.stateMachine(`${processId}:transfer:transferCircles`),
-        onDone: "#success",
-        onError: "#error"
-      }
-    },
-    callXdaiTransfer: {
-      id: "callXdaiTransfer",
-      on: <any>{
-        ...ipc("callXdaiTransfer"),
+      callCirclesTransfer: {
+        id: "callCirclesTransfer",
+        on: <any>{
+          ...ipc("callCirclesTransfer"),
+        },
+        invoke: {
+          src: transferCircles.stateMachine(`${processId}:transfer:transferCircles`),
+          onDone: "#success",
+          onError: "#error"
+        }
       },
-      invoke: {
-        src: transferXdai.stateMachine(`${processId}:transfer:transferXdai`),
-        onDone: "#success",
-        onError: "#error"
+      callXdaiTransfer: {
+        id: "callXdaiTransfer",
+        on: <any>{
+          ...ipc("callXdaiTransfer"),
+        },
+        invoke: {
+          src: transferXdai.stateMachine(`${processId}:transfer:transferXdai`),
+          onDone: "#success",
+          onError: "#error"
+        }
+      },
+      success: {
+        id: 'success',
+        type: 'final',
+        data: (context, event: PlatformEvent) => {
+          return "yeah!";
+        }
       }
     },
-    success: {
-      id: 'success',
-      type: 'final',
-      data: (context, event: PlatformEvent) => {
-        return "yeah!";
-      }
-    }
-  },
-});
+  });
 
 export const transfer: ProcessDefinition<void, TransferCirclesContextData> = {
   name: "transfer",
